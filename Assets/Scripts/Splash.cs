403ddd3350e7be0ed9c14fafd6443a7cdb5e6f97
Saeed Aliakbari <saliakbari6603@gmail.com>
2019-03-02 18:42:46 +0330

    Add GameAnalytics

---
+++ Assets/Scripts/Splash.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameAnalyticsSDK;
+
+
+public class Splash : MonoBehaviour {
+
+    public GoToScene goTo;
+
+    void Start () {
+        GameAnalytics.Initialize();
+        goTo.go("Main");
+	}
+	
+}
