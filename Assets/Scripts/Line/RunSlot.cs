42d64f07af9be21ece323d1698541a3529151571
Saeed Aliakbari <saliakbari6603@gmail.com>
2019-03-11 14:29:00 +0330

    StartSlot Spriterender is Update

--- Assets/Scripts/Line/RunSlot.cs
+++ Assets/Scripts/Line/RunSlot.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class RunSlot : MonoBehaviour
 {
     public Sprite[] empty, full;
+    public SpriteRenderer spriteRun;
     public void UpdateSprite(bool hasCar)
     {
         //int index = Superpow.Utils.GetUnlockedAirlineLevel() - 1;
         int index = ObscuredPrefs.GetInt("unlocked_line", 1) - 1;//شماره لاین استارت است که در ابتدا یک است و بعد از آن آپدیت می تواند بشود.
-        GetComponent<SpriteRenderer>().sprite = hasCar ? full[index] : empty[index];//اگر ماشینی داخلش بود یا نبود اسپرایت متفاوت است
+        spriteRun.sprite = hasCar ? full[index] : empty[index];//اگر ماشینی داخلش بود یا نبود اسپرایت متفاوت است
 
     }
 
