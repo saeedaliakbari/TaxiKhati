cd66f9f8ee20948045313c73ba118b54b885b570
mhaji1375 <mhaji1375@yahoo.com>
2019-03-16 10:48:16 +0330

    Panel Gift

---
+++ Assets/Scripts/Gift Panel/GiftPanel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GiftPanel : MonoBehaviour
+{
+    public int num;
+    public GameObject [] giftBoxes=new GameObject[5];
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void OnEnable()
+    {
+        for (int i = 0; i < giftBoxes.Length; i++)
+        {
+            giftBoxes[i].SetActive(false);
+        }
+        giftBoxes[num].SetActive(true);
+        StartCoroutine(waitForDeactive());
+    }
+    // Update is called once per frame
+    private IEnumerator waitForDeactive()
+    {
+        yield return new WaitForSeconds(3f);
+        this.gameObject.SetActive(false);
+    }
+}
